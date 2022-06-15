55101ba0dcab5b348b5bcd9ad7646f621ff4a90e
Fandi63 <100903182+Fandi63@users.noreply.github.com>
2022-06-15 17:11:40 +0200

    Create pincrack.py

---
+++ pincrack.py
@@ -0,0 +1,10 @@
+import pyautogui as spam
+import os
+import random 
+os.system("figlet PINcrack")
+input("Start cracking?")
+i = (0)
+x = (5)
+y = random.randint(0,10000000)
+while i<x:
+    spam.press(y)
